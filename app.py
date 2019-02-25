bff618fd09eec3e1ce50acb2b07dd41daf9a279a
oleg <oleg@sher.biz>
2019-02-25 20:05:24 +0200

    test commit

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'index page test'
+    return 'index page test 1'
 
 
 @app.route('/about')
 def about():
-    return 'About page test '
+    return 'About page test 1'
 
 
 if __name__ == '__main__':
