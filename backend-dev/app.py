b76550004a48c29d59cab35a0540bc8f9ab53821
oleg Sher <oleg@sher.biz>
2019-04-10 23:14:19 +0300

    app

--- backend-dev/app.py
+++ backend-dev/app.py
@@ -6,8 +6,8 @@ if __name__ == '__main__':
 
 
 app = Flask(__name__,
-            static_folder = "../../frontend-dev/static",
-            template_folder = "../../frontend-dev/dist")
+            static_folder = "../frontend-dev/dist/static",
+            template_folder = "../frontend-dev/dist")
 @app.route('/')
 def index():
     return render_template("index.html")
