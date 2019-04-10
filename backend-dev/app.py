ecf5c9189217a90b443cfe36f93ae981baa4a300
oleg Sher <oleg@sher.biz>
2019-04-10 22:41:06 +0300

    app

--- backend-dev/app.py
+++ backend-dev/app.py
@@ -1,12 +1,13 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+from flask import Flask, render_template
 
 
 if __name__ == '__main__':
-    app.run()
+        app.run()
+
+
+app = Flask(__name__,
+            static_folder = "../../frontend-dev/static",
+            template_folder = "../../frontend-dev/dist")
+@app.route('/')
+def index():
+    return render_template("index.html")
